0ca242b2f7416a7d0b8ac2bafa87925659915869
SW <swb402@yahoo.com.br>
2024-11-12 18:57:33 -0300

    # comments

--- letras_musica_API.py
+++ letras_musica_API.py
@@ -5,19 +5,18 @@ artist = "titas" # Ex.: titas, u2
 
 title = "epitafio" # Ex.: epitafio, one
 
-
+# Source of lyrics.ovh and API to search for lyrics of a song
 endpoint = f'https://api.lyrics.ovh/v1/{artist}/{title}'
 
 
 response = requests.get(endpoint)
 
-# Removendo os caracteres \n e \r
-
+# Removing the characters \n e \r
 lyrics = response.json()["lyrics"].replace("\r", "") #.replace("\n", " ")
 
 pprint.pprint(lyrics)
 
-# Salvando a letra em um arquivo texto
+# Saving the lyrics to a text file
 filename = f"{artist}_{title}.txt"
 with open(filename, "w", encoding="utf-8") as file:
     file.write(lyrics)
